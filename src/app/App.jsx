078d596fb46955f0afd83be94dd84c968a8d3f1b
petrovskyiWD <Petrovskyi.webdev@gmail.com>
2018-10-15 15:55:45 +0300

    Fix readme.md conflict

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class App extends Component {
+    state = {
+        modal: false,
+        submit: true,
+	}
+
+    render() {
+		const gg;
+        return <div>Hello</div>
+    }
+}
+
+export default App
